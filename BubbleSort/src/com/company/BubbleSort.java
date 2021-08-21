60bde3e47c7db7f2bf94e89468b4627b7e84adb7
Rafayel Shahnazaryan <60646906+Rafayel02@users.noreply.github.com>
2021-08-21 11:32:48 +0400

    BubbleSort

--- BubbleSort/src/com/company/BubbleSort.java
+++ BubbleSort/src/com/company/BubbleSort.java
@@ -1,17 +1,5 @@
 package com.company;
 
-/*
- * Explanation and complexity
-
- *  I am taking first node value and compare with second, node value and swap them if the first's node value
- * is bigger and so on till the end - 1 - count (count is steps, because every time the biggest value
- * gets it's place from back)
- *  Best case is, when the array is sorted, the time complexity will bew O(n) and worst case is reversed
- * sorted array, it will be (n-1) times take the first node value and compare n-1-counter time, which complexity
- * is o(n). So we get (n-1)n complexity steps: (n-1)n = n²-n ~ n² => The time complexity is O(n²).
- * TIME COMPLEXITY IN AVERAGE: O(n²), when array is sorted complexity is O(n).
- * */
-
 public class BubbleSort {
 
     private static int[] array = {10, 14, 28, 11, 7, 16, 1, 30, 50, 25, 18, 2, 500, 0};
